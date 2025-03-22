77f02a5c0b4229c034c6e91d5455e1355e8a0349
Kamila <kamila.matuszak@list.pl>
2025-03-22 16:31:18 +0100

    Dropdown

--- XPJam2025/Assets/Scripts/LetterController.cs
+++ XPJam2025/Assets/Scripts/LetterController.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LetterController : MonoBehaviour
 {
     [SerializeField] private KeyBindsSO letters;
     [SerializeField] private List<Transform> letterPlaces;
+    [SerializeField] private List<GameObject> platforms;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Dropdown dropdown;
 
     public void CreateTheLetters()
     {
@@ -14,6 +18,7 @@ public class LetterController : MonoBehaviour
             var prefab = Resources.Load($"Letters/{letters.keyBinds[letterPlaces.IndexOf(place)]}") as GameObject;
             var letter = Instantiate(prefab, place);
             letter.transform.position = place.position;
+            letter.transform.localScale *= slider.value;
         }
     }
 }
