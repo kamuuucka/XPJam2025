0225bec63bda3731a9d6e80927c288bbf543ee0e
Kamila <kamila.matuszak@list.pl>
2025-03-23 10:19:33 +0100

    LettersFixed

--- XPJam2025/Assets/Scripts/LetterController.cs
+++ XPJam2025/Assets/Scripts/LetterController.cs
@@ -1,40 +1,70 @@
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class LetterController : MonoBehaviour
 {
-    [SerializeField] private KeyBindsSO letters;
+    [SerializeField] private KeyBindsSO lettersToSave;
+    [SerializeField] private List<GameObject> lettersPrefabs;
     [SerializeField] private List<Transform> letterPlaces;
     [SerializeField] private List<GameObject> platforms;
     [SerializeField] private Slider slider;
     [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private GameObject scrollViewContent;
+    [SerializeField] private GameObject level2ui;
+    [SerializeField] private GameObject level3ui;
+
+    private int _level;
 
     public void CreateTheLetters()
     {
+
         foreach (var place in letterPlaces)
         {
-            var prefab = Resources.Load($"Letters/{letters.keyBinds[letterPlaces.IndexOf(place)]}") as GameObject;
-            var letter = Instantiate(prefab, place);
+            var letterPrefab = lettersPrefabs.FirstOrDefault(
+                obj => obj.name == lettersToSave.keyBinds[letterPlaces.IndexOf(place)]);
+            Debug.Log(letterPrefab.name);
+            var letter = Instantiate(letterPrefab, place);
             letter.transform.position = place.position;
-            letter.transform.localScale *= slider.value;
+            if(_level != 0) letter.transform.localScale *= slider.value;
         }
 
-        for (int i = 0; i < platforms.Count; i++)
+        if (_level == 2)
         {
-            if (i == dropdown.value * 2 || i == dropdown.value * 2 + 1)
+            for (int i = 0; i < platforms.Count; i++)
             {
-                platforms[i].SetActive(true);
+                if (i == dropdown.value * 2 || i == dropdown.value * 2 + 1)
+                {
+                    platforms[i].SetActive(true);
+                    platforms[i].transform.localScale *= slider.value;
+                }
+                else
+                {
+                    platforms[i].SetActive(false);
+                }
             }
-            else
-            {
-                platforms[i].SetActive(false);
-            }
+        
+            letterPlaces[dropdown.value].gameObject.SetActive(false);
+        }
+
+        _level++;
+        
+        if (_level == 1)
+        {
+            var level2uiobject = Instantiate(level2ui, scrollViewContent.transform);
+            level2uiobject.SetActive(true);
+            slider = level2uiobject.GetComponentInChildren<Slider>();
         }
         
-        letterPlaces[dropdown.value].gameObject.SetActive(false);
-        
+        if (_level == 2)
+        {
+            var level3uiobject = Instantiate(level3ui, scrollViewContent.transform);
+            level3uiobject.SetActive(true);
+            dropdown = level3uiobject.GetComponentInChildren<TMP_Dropdown>();
+        }
     }
 }
--- XPJam2025/Assets/Scripts/ScriptableObjects/KeyBindsSO.cs
+++ XPJam2025/Assets/Scripts/ScriptableObjects/KeyBindsSO.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "KeyBinds", menuName = "ScriptableObjects/KeyBindSO", order = 1)]
 public class KeyBindsSO : ScriptableObject
 {
-    public char[] keyBinds = new char[4];
+    public string[] keyBinds = new string[4];
 }
